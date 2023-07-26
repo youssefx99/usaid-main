b01ab517ad602fc740f4a1dfdfad0ab1e4ba5212
joue-zero <elnagaryoussef864@gmail.com>
2023-07-26 22:26:49 +0300

    Finish Home Page

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,9 @@
 /*
 Make the Carsoual Fit the Full Window 
 */
-// let Navbar = document.querySelector('.navbar');
+const CarsoualImges = document.querySelectorAll('.carousel img');
+const Navbar = document.querySelector('.navbar');
+CarsoualImges.forEach(ele => {
+    console.log(window.innerHeight);
+    ele.style.height = `${window.innerHeight - Navbar.offsetHeight}px`;
+})
