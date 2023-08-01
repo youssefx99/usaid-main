71bb86517cd6fa412df9790fdb112da6f8251c97
joue-zero <elnagaryoussef864@gmail.com>
2023-08-01 03:58:13 +0300

    the version  1 of the sign-up page

--- js/custom.js
+++ js/custom.js
@@ -7,3 +7,110 @@ CarsoualImges.forEach(ele => {
     console.log(window.innerHeight);
     ele.style.height = `${window.innerHeight - Navbar.offsetHeight}px`;
 })
+
+/*     Sign up  Page      */
+/*change The User Image Based On His Choice*/
+// Function to handle the file selection and update the image
+function handleFileSelect(event) {
+    const file = event.target.files[0];
+  
+    if (file) {
+      const reader = new FileReader();
+  
+      reader.onload = function(event) {
+        const previewImage = document.getElementById('previewImage');
+        previewImage.src = event.target.result;
+        // previewImage.style.display = 'block'; // Show the image
+      }
+  
+      reader.readAsDataURL(file);
+    }
+  }
+  
+// Add event listener to the file input
+const fileInput = document.getElementById('imageInput');
+fileInput.addEventListener('change', handleFileSelect);
+
+/* Hnadel The Input Label */
+document.querySelectorAll('.label-move input').forEach(ele  => {
+    ele.addEventListener('keyup', (cur)=>{
+        if(ele.value){
+            ele.classList.add('border-success')
+            ele.classList.remove('border-danger')
+            console.log(ele.nextElementSibling)
+            ele.nextElementSibling.classList.add('up');
+        }else{
+            ele.classList.remove('border-success')
+            ele.classList.add('border-danger')
+            ele.nextElementSibling.classList.remove('up');
+        }
+    })
+});
+
+/* Function to show the Universities and the Departement and the ajor*/
+// Load universities data from the JSON file
+let universitiesData; // Variable to store the fetched universities data
+
+// Load universities data from the JSON file
+fetch('../data/universities.json')
+  .then(response => response.json())
+  .then(data => {
+    universitiesData = data; // Store the fetched data in the variable
+    // Populate the University Select options
+    const universitySelect = document.getElementById('universitySelect');
+    universitiesData.universities.forEach(university => {
+      const option = document.createElement('option');
+      option.value = university.name;
+      option.textContent = university.name;
+      universitySelect.appendChild(option);
+    });
+  })
+  .catch(error => console.error('Error loading data:', error));
+
+const facultySelect = document.getElementById('facultySelect');
+const departmentSelect = document.getElementById('departmentSelect');
+
+// Function to populate the Faculty Select based on the selected University
+function updateFaculties() {
+  const selectedUniversity = universitySelect.value;
+  const selectedUniversityData = universitiesData.universities.find(u => u.name === selectedUniversity);
+
+  // Clear previous faculty options
+  facultySelect.innerHTML = '<option value="">Select Faculty</option>';
+
+  if (selectedUniversityData) {
+    selectedUniversityData.faculties.forEach(faculty => {
+      const option = document.createElement('option');
+      option.value = faculty.name;
+      option.textContent = faculty.name;
+      facultySelect.appendChild(option);
+    });
+  }
+}
+
+// Function to populate the Department Select based on the selected Faculty
+function updateDepartments() {
+  const selectedUniversity = universitySelect.value;
+  const selectedUniversityData = universitiesData.universities.find(u => u.name === selectedUniversity);
+  const selectedFaculty = facultySelect.value;
+
+  // Clear previous department options
+  departmentSelect.innerHTML = '<option value="">Select Department</option>';
+
+  if (selectedUniversityData) {
+    const selectedFacultyData = selectedUniversityData.faculties.find(f => f.name === selectedFaculty);
+
+    if (selectedFacultyData) {
+      selectedFacultyData.departments.forEach(department => {
+        const option = document.createElement('option');
+        option.value = department;
+        option.textContent = department;
+        departmentSelect.appendChild(option);
+      });
+    }
+  }
+}
+
+// Add event listeners to update Faculty and Department options when University or Faculty is selected
+universitySelect.addEventListener('change', updateFaculties);
+facultySelect.addEventListener('change', updateDepartments);
